d870359faae542f64f0b7502f44a90b0669b2967
Osama Bsharat <bsharat1999@gmail.com>
2024-03-04 10:29:18 +0200

    first-commit

---
+++ playground.dart
@@ -0,0 +1,21 @@
+
+import 'dart:io';
+main(){
+int ammount1 = 100;
+var ammount2=200;
+print('Amount1: $ammount1 | ammount2: $ammount2 \n');
+double dAmmount1=100.11;
+double dAmmount2=200.22;
+print('dAmmount1: $dAmmount1 | dAmmount2: $dAmmount2 \n');
+String name1 = 'Osama';
+var name2 = 'Shaheen';
+print('My name is : $name1 $name2\n');
+bool isItTrue1=true;
+var isItTrue2=false;
+print('isItTrue1 : $isItTrue1 | isItTrue2: $isItTrue2\n');
+dynamic weakVariable = 100;
+print('weak variable1: $weakVariable\n');
+weakVariable = 'Dart Programming';
+print('weak variable 2: $weakVariable');
+
+}
---
+++ printMyName.dart
@@ -0,0 +1,11 @@
+main(){
+  //Write a program to print your name in Dart.
+  print('My name Is Osama Bsharat');
+  //Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+  print("Hello I am \"Jhon Doe\" ");
+  print("Hello I am \'Jhon Doe\' ");
+  //Declare constant type of int set value 7.
+  const int myConstant=7;
+
+
+}
